255dadda90cbe7eb91c4ed3977022741856efd51
Ulyzzeus <103255220+Ulyzzeus@users.noreply.github.com>
2025-10-31 18:54:15 +0100

    Add new constants for multi locale and sample rates

--- custom_components/murfai_tts_fork/const.py
+++ custom_components/murfai_tts_fork/const.py
@@ -8,11 +8,13 @@ CONF_STYLE = "style"
 UNIQUE_ID = 'unique_id'
 
 # Optional Parameters
-CONF_FORMAT_MP3 = "format_mp3" # Boolean switch for MP3 format
+CONF_FORMAT_MP3 = 'format_mp3' # Boolean switch for MP3 format
 CONF_SAMPLE_RATE = "sample_rate"
+CONF_MULTI_NATIVE_LOCALE = 'multi_native_locale'
 CONF_LOCALE = "locale" # For multiNativeLocale
-CONF_PRONUNCIATION = "pronunciation" # For pronunciationDictionary
+CONF_PRONUNCIATION_DICTIONARY = 'pronunciation_dictionary'
 
 # You should update these lists with values from the paid API
 MODELS = ["en-US-terrell", "en-US-diane", "en-GB-duncan"] # Example voice IDs
 STYLES = ["Inspirational", "Conversational", "Promotional"]
+SAMPLE_RATES = [8000, 24000, 44100, 48000]
