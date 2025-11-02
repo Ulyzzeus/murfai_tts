b45598272ab012dc8a98452fc4590cd966537b8d
Ulyzzeus <103255220+Ulyzzeus@users.noreply.github.com>
2025-11-02 03:06:02 +0100

    Update const.py

--- custom_components/murfai_tts_dev/const.py
+++ custom_components/murfai_tts_dev/const.py
@@ -9,6 +9,6 @@ CONF_FORMAT_MP3 = 'format_mp3'
 CONF_MULTI_NATIVE_LOCALE = 'multi_native_locale'
 CONF_PRONUNCIATION_DICTIONARY = 'pronunciation_dictionary'
 CONF_SAMPLE_RATE = 'sample_rate'
+CONF_VOICE_LOCALE = 'voice_locale' # <-- Add this line
 UNIQUE_ID = 'unique_id'
-# Change this list to contain strings
 SAMPLE_RATES = ["8000", "24000", "44100", "48000"]
